50907878822b2438def6c7eae675db154571d196
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2025-03-26 13:04:57 -0600

    tipar una API y validar con Zod

--- src/Hooks/useWeather.tsx
+++ src/Hooks/useWeather.tsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import z from "zod";
+
 import { SearchType } from "../types";
 
 export default function useWeather() {
@@ -7,7 +9,28 @@ export default function useWeather() {
       const key = import.meta.env.VITE_API_KEY;
       const url = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${key}`;
       const { data } = await axios(url);
-      console.log(data)
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+
+      // Zod
+      const Weather = z.object({
+        name: z.string(),
+        main: z.object({
+          temp: z.number(),
+          temp_max: z.number(),
+          temp_min: z.number(),
+        }),
+      });
+
+      type Weather = z.infer<typeof Weather>;
+      const { data: weatherData } = await axios(weatherUrl);
+      const result = Weather.safeParse(weatherData);
+      
+      if (result.success) {
+        console.log(result.data.name);
+      }
+
     } catch (error) {
       console.log(error);
     }
